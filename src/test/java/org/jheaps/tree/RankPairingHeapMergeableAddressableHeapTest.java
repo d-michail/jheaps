8e488322816cad7c78d54320e7f0bbc0a5ba2b45
Dimitrios Michail <dimitrios.michail@gmail.com>
2019-06-27 14:25:02 +0300

    Fixed bugs

---
+++ src/test/java/org/jheaps/tree/RankPairingHeapMergeableAddressableHeapTest.java
@@ -0,0 +1,35 @@
+/*
+ * (C) Copyright 2014-2019, by Dimitrios Michail
+ *
+ * JHeaps Library
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jheaps.tree;
+
+import java.util.Comparator;
+
+import org.jheaps.MergeableAddressableHeap;
+
+public class RankPairingHeapMergeableAddressableHeapTest extends AbstractMergeableAddressableHeapTest {
+
+    protected MergeableAddressableHeap<Integer, String> createHeap() {
+        return new RankPairingHeap<Integer, String>();
+    }
+
+    @Override
+    protected MergeableAddressableHeap<Integer, String> createHeap(Comparator<Integer> comparator) {
+        return new RankPairingHeap<Integer, String>(comparator);
+    }
+
+}
